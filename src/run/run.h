e8788e03105e26a255f94113d4cac1646a58a2e7
Himanhju <152820359+Himanhju@users.noreply.github.com>
2025-03-20 00:34:17 +0000

    added a description for get_exe

--- src/run/run.h
+++ src/run/run.h
@@ -1,2 +1,11 @@
 //gets the executible file from PATH
+
+
+/**
+*	@brief	 Gets The Best Path To An Executible Given a Command.
+*	@param	 command Command To Find Executible Of.
+*	@example get_exe("ls"); returns "/usr/bin/ls" or "/bin/ls" if /usr/bin is not found.
+*	@returns Returns the Best Path With an Executible File.
+*	@warning Will Not have a Path If sh_init() Is Not Run First.
+*/
 char* get_exe(char* command);
